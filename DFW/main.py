5b26bc7e7add45a39984bf5920632ac07aafb472
brandon4423 <bneil@freedomsolarpower.com>
2022-10-11 11:07:58 -0500

    Added Austin Region _ Texas & NABR Network form

---
+++ DFW/main.py
@@ -0,0 +1,61 @@
+import gspread
+from docxtpl import DocxTemplate
+from docx2pdf import convert
+import os
+
+login = gspread.service_account(filename="Service\service_account.json")
+sheet_name = login.open("HOA")
+
+tab_lookup = sheet_name.worksheet("SEARCH_TOOL")
+
+user = os.getlogin()
+
+def acc():
+    sunrise_id = str(tab_lookup.acell("H2").value)
+
+    print(f"SUNRISE ID: {sunrise_id}")
+    forms = ['NABR']
+    print(f"ATX Forms: \n \n{forms} \n")
+    answer = input(f"Please choose the form you want: ")
+
+    if answer == 'NABR':
+        nabr()
+    elif answer == 'ID':
+        changeid()
+    else:
+        choose_again()
+
+def nabr():
+
+    hoa_name = str(tab_lookup.acell("B7").value)
+    date = str(tab_lookup.acell("H7").value)
+    name = str(tab_lookup.acell("D7").value)
+    address = str(tab_lookup.acell("B13").value)
+    phone = str(tab_lookup.acell("F2").value)
+    email = str(tab_lookup.acell("E2").value)
+
+    doc = DocxTemplate(r"Forms\\nabr.docx")
+    context = {'date': date, 'name': name, 'hoa_name': hoa_name,
+               'phone': phone, 'email': email, 'address': address}
+
+    doc.render(context)
+    os.chdir(r"C:\\Users" + "\\" + user + "\\Downloads")
+    doc.save(f"{name} NABR.docx")
+
+    convert(f"{name} NABR.docx", f"{name} NABR.pdf")
+
+    os.remove(f"{name} NABR.docx")
+    print("ACC Finished...")
+
+def changeid():
+    pass
+
+def choose_again():
+    print(f" \n\n Invalid input, please copy and paste or enter exactly \n\n")
+    acc()
+
+def main():
+    acc()
+
+if __name__ == '__main__':
+    main()
